08be8bdbbc18a20d55f5a696fc84e7056e130073
Thanhloile <tle36@huskers.unl.edu>
2020-08-20 21:04:57 +0000

    added my name and major to output

--- hello.c
+++ hello.c
@@ -1,17 +1,20 @@
 /**
  * Author: Madison Maloney
- * Date: 2020 
- * 
- * A simple hello world program 
+ * Date: 2020
+ *
+ * A simple hello world program
  */
 #include <stdlib.h>
 #include <stdio.h>
 
 int main(int argc, char **argv) {
-    
+
     printf("Madison Maloney\n");
     printf("Lorelei Trimberger\n");
     printf("Computer Science\n");
-    
+
+    printf("Thanhloi Le\n");
+    printf("Electrical Engineering\n");
+
     return 0;
 }
